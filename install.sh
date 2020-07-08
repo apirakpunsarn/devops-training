d59a9e4c2883d09a8fe0e7130b7129623e2f7b29
apirak <apirak@punsarn.asia>
2020-07-08 11:14:49 +0700

    2020-07-08

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O- https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /tmp/minikube
+cp /tmp/minikube /usr/local/bin/minikube
+chmod 755 /usr/local/bin/minikube
