b2751979bda4524cdc9d228c4c2b85eb36d2fd74
apirak <apirak@punsarn.asia>
2020-07-08 13:10:12 +0700

    2020-07-08

--- download-doc.sh
+++ download-doc.sh
@@ -5,7 +5,11 @@ HOST=172.21.0.23
 THISDIR=$(pwd)
 
 cd /tmp
-wget -rnH --reject=exe $HOST/course
+[[ -d course ]] && rm -rf course
+
+wget -rnH --reject=exe $HOST/course/
+
 find /tmp/course -type f -name "*img*" -exec rm {} \;
 find /tmp/course -type f -name "index*" -exec rm {} \;
+
 rsync -avrP /tmp/course/ $THISDIR/course
