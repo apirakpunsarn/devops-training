043ebcffea563cd34a84447df40cb8422c0feee9
apirak <apirak@punsarn.asia>
2020-07-08 11:24:37 +0700

    2020-07-08

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 wget -O- https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /tmp/minikube
 cp /tmp/minikube /usr/local/bin/minikube
 chmod 755 /usr/local/bin/minikube
+
+
+apt install virtualbox
